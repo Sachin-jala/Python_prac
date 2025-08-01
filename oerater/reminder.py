c207ab4686052623be24e11a9b781e8227fadadd
sachin-09012006 <jalasachin93@gmail.com>
2025-08-01 12:44:57 +0530

    First commit

---
+++ oerater/reminder.py
@@ -0,0 +1,4 @@
+#find reminder number devided by 2
+a=int(input("Enter the number:"))
+a%=2
+print("remainder is:",a)
---
+++ oerater/substraction.py
@@ -0,0 +1,5 @@
+#add two number
+a=int(input("Enter a number:"))
+b=int(input("Enter another number:"))
+c=a-b
+print("the sun of two no is:",c)
